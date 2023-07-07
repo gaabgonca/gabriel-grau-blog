5dfd5d4c0facc99ad270e3b678b54b1a4a916296
Gabriel Gonzalez <gaabgonca@gmail.com>
2023-07-06 22:35:42 -0500

    Add analytics for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,11 +12,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		version: {
-            name: child_process.execSync('git rev-parse HEAD').toString().trim()
-        }
 	}
-	
+
 };
 
 export default config;
